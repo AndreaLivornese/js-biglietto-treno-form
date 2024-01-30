0674f93e966d5eef0a4d90d1419ea5dc549cfce1
andrea-livornese <andrealivornese@gmail.com>
2024-01-30 15:58:39 +0100

    creato script per inserire dati nel biglietto finale

--- js/script.js
+++ js/script.js
@@ -30,17 +30,47 @@ buttonElemnt.addEventListener("click",
             const discount =  price * 40 / 100;
             totalPrice= price - discount;
             document.getElementById("result").innerHTML = totalPrice.toFixed(2) + "€";
+            
+            document.querySelector("#type").innerHTML = "Senior -40%";
 
         }else if (age <18) {
             
             const discount =  price * 20 / 100;
             totalPrice= price - discount;
             document.getElementById("result").innerHTML = totalPrice.toFixed(2) + "€";
+            document.querySelector("#type").innerHTML = "Young -20%";
 
         } else {
 
             document.getElementById("result").innerHTML = price.toFixed(2) + "€";
+
+            document.querySelector("#type").innerHTML = "Standard";
         }
     }
+)
+
+
+
+
+buttonElemnt.addEventListener("click",
+    function(){
+        let fullName= document.querySelector("#fullName").value;
+        document.querySelector("#ticketFullName").innerHTML = fullName;
+
+
+        const codeCP= Math.floor(Math.random() * 90000 + 10000); //genera un numero casuale da 10000 a 99999 (per il codice CP)
+
+        console.log(codeCP);
+        document.querySelector("#code").innerHTML = codeCP;
+
+
+
+        const carrozza= Math.floor(Math.random() * 20 + 1); //genera un numero casuale da 1 a 20 (per la carrozza)
+
+        console.log(carrozza);
+        document.querySelector("#carriage").innerHTML = carrozza;
+    }
+
+
 )
 
